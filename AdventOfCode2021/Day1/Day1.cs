46664c0677b01e43cb9b124d41efbbb09e2cc0a3
Tyler Standridge <tstand90@gmail.com>
2021-12-01 18:29:54 -0800

    Initial commit (Day 1 complete)

---
+++ AdventOfCode2021/Day1/Day1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Day1
+{
+    public static class Day1
+    {
+        private static List<int> GetInput()
+        {
+            var lines = File.ReadAllLines("Day1/input.txt");
+            
+            var measurements = lines.Select(int.Parse).ToList();
+
+            return measurements;
+        }
+        
+        public static int PartOne()
+        {
+            var numberOfTimesDepthIncreased = 0;
+
+            var measurements = GetInput();
+
+            for (var i = 1; i < measurements.Count; i++)
+            {
+                if (measurements[i] > measurements[i - 1])
+                {
+                    numberOfTimesDepthIncreased++;
+                }
+            }
+
+            return numberOfTimesDepthIncreased;
+        }
+
+        public static int PartTwo()
+        {
+            var numberOfTimesDepthIncreased = 0;
+            
+            var measurements = GetInput();
+            var sums = new List<int>();
+
+            for (var i = 0; i < measurements.Count - 2; i++)
+            {
+                sums.Add(measurements[i] + measurements[i + 1] + measurements[i + 2]);
+            }
+
+            for (var i = 1; i < sums.Count; i++)
+            {
+                if (sums[i] > sums[i - 1])
+                {
+                    numberOfTimesDepthIncreased++;
+                }
+            }
+            
+            return numberOfTimesDepthIncreased;
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using AdventOfCode2021.Day1;
+
+Console.WriteLine($"Day 1 | Part 1: {Day1.PartOne()}");
+Console.WriteLine($"Day 1 | Part 2: {Day1.PartTwo()}");
