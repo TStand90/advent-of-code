09584a3c28686e2e704a4ebc91f0eccf6be98f60
Tyler Standridge <tstand90@gmail.com>
2021-12-02 07:37:19 -0800

    Day 2 complete.

---
+++ AdventOfCode2021/Day2/Day2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Day2
+{
+    public static class Day2
+    {
+        private static List<Tuple<string, int>> GetInput()
+        {
+            var lines = File.ReadAllLines("Day2/input.txt");
+
+            return lines.Select(
+                line => line.Split(' '))
+                .Select(splitLine => 
+                new Tuple<string, int>(splitLine[0], int.Parse(splitLine[1])))
+                .ToList();
+        }
+        
+        public static int PartOne()
+        {
+            var verticalPosition = 0;
+            var horizontalPosition = 0;
+            
+            var inputs = GetInput();
+
+            foreach (var (direction, unit) in inputs)
+            {
+                switch (direction)
+                {
+                    case "forward":
+                        horizontalPosition += unit;
+                        break;
+                    case "up":
+                        verticalPosition -= unit;
+                        break;
+                    case "down":
+                        verticalPosition += unit;
+                        break;
+                }
+            }
+            
+            return verticalPosition * horizontalPosition;
+        }
+
+        public static int PartTwo()
+        {
+            var verticalPosition = 0;
+            var horizontalPosition = 0;
+            var aim = 0;
+            
+            var inputs = GetInput();
+            
+            foreach (var (direction, unit) in inputs)
+            {
+                switch (direction)
+                {
+                    case "forward":
+                        horizontalPosition += unit;
+                        verticalPosition += aim * unit;
+                        break;
+                    case "up":
+                        aim -= unit;
+                        break;
+                    case "down":
+                        aim += unit;
+                        break;
+                }
+            }
+            
+            return verticalPosition * horizontalPosition;
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -2,6 +2,10 @@
 
 using System;
 using AdventOfCode2021.Day1;
+using AdventOfCode2021.Day2;
 
 Console.WriteLine($"Day 1 | Part 1: {Day1.PartOne()}");
 Console.WriteLine($"Day 1 | Part 2: {Day1.PartTwo()}");
+
+Console.WriteLine($"Day 2 | Part 1: {Day2.PartOne()}");
+Console.WriteLine($"Day 2 | Part 2: {Day2.PartTwo()}");
