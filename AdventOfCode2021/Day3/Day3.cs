278a403be3ebd0e9ab76ea59fee4c43cdd09847e
Tyler Standridge <tstand90@gmail.com>
2021-12-03 08:31:07 -0800

    Day 3 complete

---
+++ AdventOfCode2021/Day3/Day3.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Day3
+{
+    public static class Day3
+    {
+        private static List<string> GetInput()
+        {
+            var lines = File.ReadAllLines("Day3/input.txt");
+
+            return lines.ToList();
+        }
+
+        private static Dictionary<int, Dictionary<int, int>> GetCounts(List<string> binaryStrings)
+        {
+            var counts = new Dictionary<int, Dictionary<int, int>>();
+
+            for (var index = 0; index < binaryStrings[0].Length; index++)
+            {
+                counts[index] = new Dictionary<int, int>
+                {
+                    [0] = 0,
+                    [1] = 0
+                };
+            }
+
+            foreach (var binaryString in binaryStrings)
+            {
+                for (var index = 0; index < binaryString.Length; index++)
+                {
+                    var binaryCharacter = binaryString[index];
+
+                    if (binaryCharacter == '0')
+                    {
+                        counts[index][0] += 1;
+                    }
+                    else
+                    {
+                        counts[index][1] += 1;
+                    }
+                }
+            }
+
+            return counts;
+        }
+
+        public static int PartOne()
+        {
+            var inputs = GetInput();
+
+            var inputLength = inputs[0].Length;
+
+            var counts = GetCounts(inputs);
+
+            var gammaRateString = "";
+            var epsilonRateString = "";
+
+            for (var i = 0; i < inputLength; i++)
+            {
+                var countForPlace = counts[i];
+
+                var zeroCount = countForPlace[0];
+                var oneCount = countForPlace[1];
+                
+                if (zeroCount > oneCount)
+                {
+                    gammaRateString += '0';
+                    epsilonRateString += '1';
+                }
+                else
+                {
+                    gammaRateString += '1';
+                    epsilonRateString += '0';
+                }
+            }
+            
+            var gammaRate = Convert.ToInt32(gammaRateString, 2);
+            var epsilonRate = Convert.ToInt32(epsilonRateString, 2);
+            
+            return gammaRate * epsilonRate;
+        }
+
+        public static int PartTwo()
+        {
+            int GetRating(List<string> binaryStrings, bool isOxygenRating, int currentPosition = 0)
+            {
+                if (binaryStrings.Count == 1)
+                {
+                    return Convert.ToInt32(binaryStrings[0], 2);
+                }
+                
+                var counts = GetCounts(binaryStrings);
+
+                var countsForPosition = counts[currentPosition];
+
+                var zeroCount = countsForPosition[0];
+                var oneCount = countsForPosition[1];
+
+                char value;
+
+                if (isOxygenRating)
+                {
+                    value = zeroCount > oneCount ? '0' : '1';
+                }
+                else
+                {
+                    value = zeroCount <= oneCount ? '0' : '1';
+                }
+                
+                List<string> reducedList = binaryStrings.Where(binaryString => binaryString[currentPosition] == value).ToList();
+
+                return GetRating(reducedList, isOxygenRating, currentPosition + 1);
+            }
+            
+            var inputs = GetInput();
+
+            var inputLength = inputs[0].Length;
+
+            var counts = new Dictionary<int, Dictionary<int, int>>();
+
+            for (var index = 0; index < inputLength; index++)
+            {
+                counts[index] = new Dictionary<int, int>
+                {
+                    [0] = 0,
+                    [1] = 0
+                };
+            }
+
+            foreach (var input in inputs)
+            {
+                for (var index = 0; index < input.Length; index++)
+                {
+                    var binaryCharacter = input[index];
+
+                    if (binaryCharacter == '0')
+                    {
+                        counts[index][0] += 1;
+                    }
+                    else
+                    {
+                        counts[index][1] += 1;
+                    }
+                }
+            }
+
+            var oxygenGeneratorRating = GetRating(inputs, true);
+            var c02ScrubberRating = GetRating(inputs, false);
+            
+            return oxygenGeneratorRating * c02ScrubberRating;
+        }
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,11 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Collections.Generic;
+using System.Reflection;
+
 using AdventOfCode2021.Day1;
 using AdventOfCode2021.Day2;
+using AdventOfCode2021.Day3;
+
+List<int> daysToRun = new();
+
+foreach (var arg in args)
+{
+    try
+    {
+        daysToRun.Add(int.Parse(arg));
+    }
+    catch
+    {
+        Console.WriteLine($"Invalid argument: ${arg}, please give only a space separated list of numbers");
+    }
+    
+    Console.WriteLine(arg);
+}
+
+foreach (var dayToRun in daysToRun)
+{
+    var t = Type.GetType($"Day{dayToRun}");
+    
+    MethodInfo method = t.GetMethod("PartOne", BindingFlags.Static | BindingFlags.Public);
+
+    method.Invoke(null, null);
+}
 
 Console.WriteLine($"Day 1 | Part 1: {Day1.PartOne()}");
 Console.WriteLine($"Day 1 | Part 2: {Day1.PartTwo()}");
 
 Console.WriteLine($"Day 2 | Part 1: {Day2.PartOne()}");
 Console.WriteLine($"Day 2 | Part 2: {Day2.PartTwo()}");
+
+Console.WriteLine($"Day 3 | Part 1: {Day3.PartOne()}");
+Console.WriteLine($"Day 3 | Part 2: {Day3.PartTwo()}");
